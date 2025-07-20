801c6bf4ac3a53483a6c6a904ec3719301057421
João Mendes <victor.joaomendes@outlook.com>
2025-07-20 14:26:48 -0300

    Iniciar projeto

---
+++ js/app.js
@@ -0,0 +1,65 @@
+//  sortear os nomes ao clicar no botao sortear
+// mostrar quem foi os sorteados
+// reiniciar ao clicar em reiniciar
+
+let sorteador = [];
+function adicionar(nomes) {
+  let amigo = document.getElementById("nome-amigo");
+  let lista = document.getElementById("lista-amigos");
+  sorteador.push(amigo.value);
+  if (amigo.value == " ") {
+    alert("Escreva um nome para ser adicionado!");
+  }
+    if (lista.textContent == "") {
+      lista.textContent = amigo.value;
+    } else {
+      lista.textContent = lista.textContent + ", " + amigo.value;
+    }
+    amigo.value = " ";
+  }
+
+function sortear() {
+  embaralhar(sorteador);
+  let sorteio = document.getElementById("lista-sorteio");
+  sorteador = sorteador.filter((nome) => nome.trim() !== "");
+  if (sorteador.length < 1) {
+    alert("Adicione 3 nomes para sortear!");
+  } else {
+    if (sorteador.length < 3) {
+      let contar;
+      contar = 3 - sorteador.length;
+      let palavras = sorteador.length === 1 ? "nomes" : "nome";
+      alert(`Adicione ${contar} ${palavras} para sortear!`);
+    } else {
+      for (let i = 0; i < sorteador.length; i++) {
+        if (i == sorteador.length - 1) {
+          sorteio.innerHTML =
+            sorteio.innerHTML + sorteador[i] + " --> " + sorteador[0] + "<br/>";
+        } else {
+          sorteio.innerHTML =
+            sorteio.innerHTML +
+            sorteador[i] +
+            " --> " +
+            sorteador[i + 1] +
+            "<br/>";
+        }
+      }
+    }
+  }
+}
+
+function embaralhar(lista) {
+  for (let indice = lista.length; indice; indice--) {
+    const indiceAleatorio = Math.floor(Math.random() * indice);
+    [lista[indice - 1], lista[indiceAleatorio]] = [
+      lista[indiceAleatorio],
+      lista[indice - 1],
+    ];
+  }
+}
+
+function reiniciar() {
+  sorteador = [];
+  document.getElementById("lista-amigos").innerHTML = "";
+  document.getElementById("lista-sorteio").innerHTML = "";
+}
